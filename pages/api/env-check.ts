3fa0cce38dda96b01b5b103cb214da3322603efd
GestionBBDD-Hexalud <gestionbbdd@hexalud.com>
2025-10-07 19:00:34 -0600

    Update env-check.ts

--- pages/api/env-check.ts
+++ pages/api/env-check.ts
@@ -5,6 +5,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     AIRTABLE_API_KEY: Boolean(process.env.AIRTABLE_API_KEY),           // sólo indica si existe
     AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID?.slice(0, 3) + "...",
     AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
-    MAPBOX_TOKEN_pk: process.env.MAPBOX_TOKEN?.startsWith("pk.") || false
+    MAPBOX_TOKEN_pk: process.env.MAPBOX_TOKEN?.startsWith("pk.eyJ1IjoiaGV4YWx1ZCIsImEiOiJjbWV0Ym53amIwY241MmptemZ6cHBlcGJ0In0.gsj69pAvQqcibLTJAuHd5A") || false
   });
 }
